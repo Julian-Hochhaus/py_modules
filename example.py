280d094ff76f9f413a161177548c96b16c8d87c4
Julian-Hochhaus <julian.hochhaus@tu-dortmund.de>
2018-04-27 10:10:28 +0200

    implented the possibility to use unp.uarrays too

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
 from modules.table import textable
-
+import uncertainties.unumpy as unp
 #example textable
-arr1=[0.4,0.75,1.4]
-arr2=[2,3,4]
+arr2= unp.uarray([1.095473487583, 2,3], [0.0112445, 0.002,0.2])
+arr1=[2,3,4]
 textable.latex_tab(data=[arr1,arr2],names=[r"title column 1",r"title column 2"], filename=r"example.tex",caption=r"Beautiful caption",label=r"important_label",dec_points=[2,0])
-# dec_points sets precision, i.e. dec_points[0]=2 will display 2 decimal places for all values in column 1 
+# dec_points sets precision, i.e. dec_points[0]=2 will display 2 decimal places for all values in column 1
+# for values with uncertainties, the given value in dec_points is ignored. Instead, it follows the rounding rules defined by the Particle Data Group.  
--- modules/table/textable.py
+++ modules/table/textable.py
@@ -1,3 +1,4 @@
+import uncertainties
 def latex_tab(data=[[1,2,3],[42,42,42]],names=["col1","col2"],filename="test.tex",caption="Caption",label="test", dec_points=[0,2]):
     try:
         for i in range(len(data)-1):
@@ -10,12 +11,12 @@ def latex_tab(data=[[1,2,3],[42,42,42]],names=["col1","col2"],filename="test.tex
             raise TypeError("data and names and dec_points must have same dimension! "+"len(data)= "+str(len(data))+"; len(names)= "+str(len(names)) +"; len(dec_points)= "+str(len(dec_points)))
     except TypeError: raise
     else:
-        texfile = open(filename,"w")
+	texfile = open(filename,"w")
         texfile.write("\\begin{table}\n");
         texfile.write(" \\caption{"+caption+"}\n");
         texfile.write(" \\label{tab:"+label+"}\n")
         texfile.write(" \\centering\n")
-	texfile.write("\\sisetup{table-format=1.1}")
+        texfile.write("\\sisetup{table-format=1.1}")
         texfile.write(" \\begin{tabular}{")
         for col in data:
             texfile.write("S")
@@ -29,8 +30,17 @@ def latex_tab(data=[[1,2,3],[42,42,42]],names=["col1","col2"],filename="test.tex
         for i in range(len(data[0])):
             texfile.write("     ")
             for j in range(len(data)-1):
-                texfile.write(("{:10.%df}"%dec_points[j]).format(data[j][i])+" & ")
-            texfile.write(("{:10.%df}"%dec_points[len(dec_points)-1]).format(data[len(data)-1][i]))
+
+                if isinstance(data[j][i],uncertainties.core.Variable):
+                    data[j][i]=(str(data[j][i])).replace('+/-',' \pm ')
+                    texfile.write('$\\num{'+ data[j][i]+'}$')
+                else:
+                    texfile.write(("{:10.%df}"%dec_points[j]).format(data[j][i])+" & ")
+            if isinstance(data[len(data)-1][i],uncertainties.core.Variable):
+                data[len(data)-1][i]=(str(data[len(data)-1][i])).replace('+/-','\pm')
+                texfile.write('$\\num{'+ data[len(data)-1][i]+'}$')
+            else:
+                texfile.write(("{:10.%df}"%dec_points[len(dec_points)-1]).format(data[len(data)-1][i]))
             texfile.write(" \\\\\n")
         texfile.write(" \\bottomrule\n");
         texfile.write(" \\end{tabular}\n");
@@ -47,3 +57,4 @@ if __name__ == "__main__":
     print("42 is the answer to life the universe and everything!")
     print("Running this module as script generates a sample table.tex")
     latex_tab([arr1,arr2],["col1","col2"],"table.tex",'caption','sample')
+

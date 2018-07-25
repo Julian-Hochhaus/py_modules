e6497bdbbca6c8b23c17523c37d7ac7431011018
Julian-Hochhaus <julian.hochhaus@tu-dortmund.de>
2018-07-25 15:58:16 +0200

    removed tableformat

--- example.py
+++ example.py
@@ -6,10 +6,9 @@ import numpy as np
 #example textable
 arr2= unp.uarray([1.09553, 2,3], [0.0112445, 0.00244,0.2])
 arr1=[2,3]
-textable.latex_tab(data=[arr1,arr2],names=[r"title column 1",r"title column 2"], filename=r"example.tex",caption=r"Beautiful caption",label=r"important_label",dec_points=[2,0], tableformat=3.3)
+textable.latex_tab(data=[arr1,arr2],names=[r"title column 1",r"title column 2"], filename=r"example.tex",caption=r"Beautiful caption",label=r"important_label",dec_points=[2,0])
 # dec_points sets precision, i.e. dec_points[0]=2 will display 2 decimal places for all values in column 1
 # for values with uncertainties, the given value in dec_points is ignored. Instead, it follows the rounding rules defined by the Particle Data Group.
-#tableformat sets global column-width
 
 
 

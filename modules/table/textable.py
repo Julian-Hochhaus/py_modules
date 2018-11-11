8f6af6c9d04be63349972be729891b78f682146e
Julian-Hochhaus <julian.hochhaus@tu-dortmund.de>
2018-11-11 10:50:20 +0100

    private functions

--- modules/table/textable.py
+++ modules/table/textable.py
@@ -1,157 +1,161 @@
 import uncertainties
 import numpy as np
 import os
-def long_tab(data=[[1,2,3],[42,42,42]],names=["col1","col2"],filename="test.tex",caption="Caption",label="test", dec_points=[0,2]):
+
+#######
+#private functions
+#######
+def __write_seperator_or_newline(data,texfile,j):
+    if(j==(len(data)-1)):
+        texfile.write("\\")
+    else:
+        texfile.write("&")
+#*********
+#Write tables body
+#*********
+
+def __writedata(data,names,texfile,caption,label, dec_points,len_data_max):
+            for i in range(len_data_max):
+                texfile.write("     ")
+                    #writing all data except the last columns
+                for j in range(len(data)):
+                    if isinstance(data[j][i],uncertainties.core.Variable) or (isinstance(data[j][i],uncertainties.core.AffineScalarFunc)):
+                        if(str(data[j][i])=="0.0+/-0"):
+                            texfile.write('$\\num{'+'0'+'}$')
+                            __write_seperator_or_newline(data,texfile,j);
+                        else:
+                            value,unc=(str(data[j][i])).split('+/-')
+                            texfile.write('$\\num{'+("{:10.%df}"%dec_points[j]).format(float(value))+"\pm"+("{:10.%df}"%dec_points[j]).format(float(unc))+'}$')
+                            __write_seperator_or_newline(data,texfile,j);
+                    else:
+                        if(data[j][i]=='-'):
+                            texfile.write("$\\text{\\textbf{---}}$")
+                            __write_seperator_or_newline(data,texfile,j);
+                        else:
+                            texfile.write(("{:10.%df}"%dec_points[j]).format(float(data[j][i])))
+                            __write_seperator_or_newline(data,texfile,j);
+                texfile.write(" \\\\\n")
+
+#*********
+#write dashes to shorter arrays
+#*********
+
+def __append_dashes_to_short_arrays(data, len_data_max):
+    for i in range(len(data)):
+        if not len_data_max==len(data[i]):
+            for j in range(len_data_max-len(data[i])):
+                data[i]=np.append(data[i],'-')
+
+#*********
+#returns max lenght of data arrays
+#*********
+
+def __max_array_lenght(data):
+    len_data_arr=[]
+    for i in range(len(data)):
+        len_data_arr.append(len(data[i]))
+    len_data_max=max(len_data_arr)
+    return len_data_max
+
+#*********
+#write column heads
+#*********
+
+def __column_names(names,texfile):
+    for i in range(len(names)-1):
+        texfile.write("{"+names[i]+"}& ")
+    texfile.write("{"+names[len(names)-1]+"}\\\ \n")
+
+#*********
+#write format string
+#*********
+
+def __write_format_string(data,format_string,texfile):
+    if format_string==None:
+        for col in data:
+            texfile.write("S")
+    else:
+        texfile.write(format_string)
+    texfile.write("}\n");
+
+###############
+#*********
+#Error function
+#*********
+
+def __test_error(data,dec_points,names):
+    for i in range(len(data)-1):
+        if not(len(names)==len(data) and len(dec_points)==len(data)):
+            raise TypeError("data and names and dec_points must have same dimension! "+"len(data)= "+str(len(data))+"; len(names)= "+str(len(names)) +"; len(dec_points)= "+str(len(dec_points)))
+
+#######
+#private functions
+########
+#*******
+######
+
+
+def long_tab(data=[[1,2,3],[42,42,42]],names=["col1","col2"],filename="test.tex",caption="Caption",label="test", dec_points=[0,2],format_string=None):
     try: #test, if names and data and dec_points array have different lenghts
-        for i in range(len(data)-1):
-            if not(len(names)==len(data) and len(dec_points)==len(data)):
-                raise TypeError("data and names and dec_points must have same dimension! "+"len(data)= "+str(len(data))+"; len(names)= "+str(len(names)) +"; len(dec_points)= "+str(len(dec_points)))
+        __test_error(data,dec_points,names)
     except TypeError: raise
     else:
         data=np.copy(data)
         #appends em dashs to shorter data arrays
-        len_data_arr=[]
-        for i in range(len(data)):
-            len_data_arr.append(len(data[i]))
-        len_data_max=max(len_data_arr)
-        for i in range(len(data)):
-            if not len_data_max==len(data[i]):
-                for j in range(len_data_max-len(data[i])):
-                    data[i]=np.append(data[i],'-')
+        len_data_max=__max_array_lenght(data)
+        __append_dashes_to_short_arrays(data,len_data_max)
         #start writing table
         texfile = open(filename,"w")
-        texfile.write("\\sisetup{detect-all}\n")#setup columnwidth to fit best
+        texfile.write("\\sisetup{detect-all, parse-numbers=false}\n")#setup columnwidth to fit best
         texfile.write(" \\begin{longtable}{")
-        for col in data:
-            texfile.write("S")
-        texfile.write("}\n");
+        __write_format_string(data,format_string,texfile);
         texfile.write(" \\caption{"+caption+"}\n");
         texfile.write(" \\label{tab:"+label+"}\\\ \n")
         texfile.write(" \\toprule"+"\n");
-        for i in range(len(names)-1):
-            texfile.write("{"+names[i]+"}& ")
-        texfile.write("{"+names[len(names)-1]+"}\\\ \n")
+        __column_names(names,texfile)
         texfile.write("\\midrule\n");
-
         texfile.write("\\endfirsthead"+"\n")
         texfile.write(" \\toprule"+"\n");
         #writing column titles
-        for i in range(len(names)-1):
-            texfile.write("{"+names[i]+"}& ")
-        texfile.write("{"+names[len(names)-1]+"}\\\ \n")
+        __column_names(names,texfile)
         texfile.write("\\midrule\n");
         texfile.write("\\endhead"+"\n")
         texfile.write("\\midrule\n");
         texfile.write("\\endfoot")
         texfile.write(" \\bottomrule\n");
         texfile.write("\\endlastfoot");
-
-        #writing data
-        for i in range(len_data_max):
-            texfile.write("     ")
-            #writing all data except the last columns
-            for j in range(len(data)-1):
-                if isinstance(data[j][i],uncertainties.core.Variable) or (isinstance(data[j][i],uncertainties.core.AffineScalarFunc)):
-                    if(str(data[j][i])=="0.0+/-0"):
-                        texfile.write('$\\num{'+'0'+'}$'+"&")
-                    else:
-                        data[j][i]=(str(data[j][i])).replace('+/-','\pm')
-                        texfile.write('$\\num{'+ data[j][i]+'}$'+"&")
-                else:
-                    if(data[j][i]=='-'):
-                        texfile.write("$\\text{\\textbf{---}}$"+"&")
-                    else:
-                        texfile.write(("{:10.%df}"%dec_points[j]).format(float(data[j][i]))+" & ")
-            #writing last column, seperated to get \n at the end
-            if isinstance(data[len(data)-1][i],uncertainties.core.Variable) or (isinstance(data[len(data)-1][i],uncertainties.core.AffineScalarFunc)):
-                if(str(data[len(data)-1][i])=="0.0+/-0"):
-                    texfile.write('$\\num{'+'0'+'}$')
-                else:
-                    data[len(data)-1][i]=(str(data[len(data)-1][i])).replace('+/-','\pm')
-                    texfile.write('$\\num{'+ data[len(data)-1][i]+'}$')
-            else:
-                if(data[len(data)-1][i]=='-'):
-                    texfile.write("$\\text{\\textbf{---}}$")
-                else:
-                    texfile.write(("{:10.%df}"%dec_points[len(dec_points)-1]).format(float(data[len(data)-1][i])))
-            texfile.write(" \\\\\n")
+        __writedata(data,names,texfile,caption,label, dec_points,len_data_max)
         texfile.write(" \\end{longtable}\n");
         texfile.close()
 ############################################
 ####
-#
-#
-#
-#
+####
+############################################
 
-
-
-
-
-
-def latex_tab(data=[[1,2,3],[42,42,42]],names=["col1","col2"],filename="test.tex",caption="Caption",label="test", dec_points=[0,2]):
+def latex_tab(data=[[1,2,3],[42,42,42]],names=["col1","col2"],filename="test.tex",caption="Caption",label="test", dec_points=[0,2], format_string=None):
     try: #test, if names and data and dec_points array have different lenghts
-        for i in range(len(data)-1):
-            if not(len(names)==len(data) and len(dec_points)==len(data)):
-                raise TypeError("data and names and dec_points must have same dimension! "+"len(data)= "+str(len(data))+"; len(names)= "+str(len(names)) +"; len(dec_points)= "+str(len(dec_points)))
+        __test_error(data,dec_points,names)
     except TypeError: raise
     else:
+        data=np.copy(data)
         #appends em dashs to shorter data arrays
-        len_data_arr=[]
-        for i in range(len(data)):
-            len_data_arr.append(len(data[i]))
-        len_data_max=max(len_data_arr)
-        for i in range(len(data)):
-            if not len_data_max==len(data[i]):
-                for j in range(len_data_max-len(data[i])):
-                    data[i]=np.append(data[i],'-')
+        len_data_max=__max_array_lenght(data)
+        __append_dashes_to_short_arrays(data,len_data_max)
         #start writing table
         texfile = open(filename,"w")
         texfile.write("\\begin{table}\n");
         texfile.write(" \\caption{"+caption+"}\n");
         texfile.write(" \\label{tab:"+label+"}\n")
         texfile.write(" \\centering\n")
-        texfile.write("\\sisetup{detect-all}\n")#setup columnwidth to fit best
+        texfile.write("\\sisetup{detect-all, parse-numbers=false}\n")#setup columnwidth to fit best
         texfile.write(" \\begin{tabular}{")
-        for col in data:
-            texfile.write("S")
-        texfile.write("}\n");
+        __write_format_string(data,format_string,texfile)
         texfile.write(" \\toprule \n    ");
         #writing column titles
-        for i in range(len(names)-1):
-            texfile.write("{"+names[i]+"}& ")
-        texfile.write("{"+names[len(names)-1]+"}")
-        texfile.write(" \\\\\n");
+        __column_names(names,texfile)
         texfile.write("     \\midrule\n");
         #writing data
-        for i in range(len_data_max):
-            texfile.write("     ")
-            #writing all data except the last columns
-            for j in range(len(data)-1):
-                if isinstance(data[j][i],uncertainties.core.Variable) or (isinstance(data[j][i],uncertainties.core.AffineScalarFunc)):
-                    if(str(data[j][i])=="0.0+/-0"):
-                        texfile.write('$\\num{'+'0'+'}$'+"&")
-                    else:
-                        data[j][i]=(str(data[j][i])).replace('+/-','\pm')
-                        texfile.write('$\\num{'+ data[j][i]+'}$'+"&")
-                else:
-                    if(data[j][i]=='-'):
-                        texfile.write("$\\text{\\textbf{---}}$"+"&")
-                    else:
-                        texfile.write(("{:10.%df}"%dec_points[j]).format(float(data[j][i]))+" & ")
-            #writing last column, seperated to get \n at the end
-            if isinstance(data[len(data)-1][i],uncertainties.core.Variable) or (isinstance(data[len(data)-1][i],uncertainties.core.AffineScalarFunc)):
-                if(str(data[len(data)-1][i])=="0.0+/-0"):
-                    texfile.write('$\\num{'+'0'+'}$')
-                else:
-                    data[len(data)-1][i]=(str(data[len(data)-1][i])).replace('+/-','\pm')
-                    texfile.write('$\\num{'+ data[len(data)-1][i]+'}$')
-            else:
-                if(data[len(data)-1][i]=='-'):
-                    texfile.write("$\\text{\\textbf{---}}$")
-                else:
-                    texfile.write(("{:10.%df}"%dec_points[len(dec_points)-1]).format(float(data[len(data)-1][i])))
-            texfile.write(" \\\\\n")
+        __writedata(data,names,texfile,caption,label, dec_points,len_data_max)
         texfile.write(" \\bottomrule\n");
         texfile.write(" \\end{tabular}\n");
         texfile.write("\\end{table}");
